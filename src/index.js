5c8c8b327accfbbf1e1ac7ea7b38a8cc2c63c865
Ryan Erricson <ryanerricson@gmail.com>
2021-05-05 16:51:54 -0400

    commit tree 6b9b6d8bfb867761552b0a6a3f899c60467d0432

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchImages()
+    fetchBreeds()
+})
+    
+function fetchImages(){
+    fetch("https://dog.ceo/api/breeds/image/random/4") 
+    .then(response => response.json())
+    .then(data => addImageToDom(data))
+}
+
+function addImageToDom(data){
+
+    imgContainer = document.getElementById("dog-image-container")
+
+    for (const e of data['message']) {
+        const img = document.createElement("img")
+        img.src = e
+        imgContainer.appendChild(img)
+    }
+}
+
+function fetchBreeds(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => addBreedsToDom(data))
+}
+
+function addBreedsToDom(data){
+
+    breedList = document.getElementById("dog-breeds")
+
+    for (breed in data['message']) {
+        let li = document.createElement("li")
+        li.innerText = breed
+        li.click = true
+        breedList.appendChild(li)
+        li.addEventListener("click", function(e) {
+            e.target.style.color = "red";
+        })
+    }
+}
